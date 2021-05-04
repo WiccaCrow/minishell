3cb7b4660e77c9e78531225b215ec9bfc2aa00f4
grvelva <grvelva@student.21-school.ru>
2021-05-04 16:19:32 +0300

    отделил функцию отображения имени программы

--- project/minishell.c
+++ project/minishell.c
@@ -1,24 +1,27 @@
 #include "./includes/minishell.h"
 
+int show_program_name()
+{
+	write(STDOUT_FILENO, GOLD, 8);
+	write(STDOUT_FILENO, NAME, ft_strlen(NAME));
+	write(STDOUT_FILENO, NONECOLOR, 5);
+	return (0);
+}
+
 int main(void)
 {
 	char	*command;
 	char	*line;
 	int		ret;
 
-	
-	write(STDOUT_FILENO, GOLD, 8);
-	write(STDOUT_FILENO, NAME, ft_strlen(NAME));
-	write(STDOUT_FILENO, NONECOLOR, 5);
+	show_program_name();
 	ret = get_next_line(1, &line);
 	while (ret != -1)
 	{
 		command = parser(line);
 		executor(command);
 		printf("Hi from main, command:\n\"%s\"\n", command);
-		write(STDOUT_FILENO, GOLD, 8);
-		write(STDOUT_FILENO, NAME, ft_strlen(NAME));
-		write(STDOUT_FILENO, NONECOLOR, 5);
+		show_program_name();
 		ret = get_next_line(1, &line);
 	}
 }
