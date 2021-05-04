64974caad9d30f84a1284fa7076cde975f3b09ed
grvelva <grvelva@student.21-school.ru>
2021-05-04 15:04:06 +0300

    Сделал вывод имени программы (золотые буквы на черном фоне)

--- includes/minishell.h
+++ includes/minishell.h
@@ -5,6 +5,10 @@
 # include "../libft/libft.h"
 # include "../gnl_dir/get_next_line.h"
 
+# define NAME "DULCIE&GRVELVA > "
+# define NONECOLOR "\x1B[0m"
+# define GOLD "\033[40;33m"
+
 char				*parser(char *line);
 int 				executor(char *line);
 
