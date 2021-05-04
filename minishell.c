3e60826e64fc86380d6278eb12ca3a919e6954d4
grvelva <grvelva@student.21-school.ru>
2021-05-04 15:07:02 +0300

    доработал вывод=)

--- minishell.c
+++ minishell.c
@@ -7,18 +7,18 @@ int main(void)
 	int		ret;
 
 	
-	write(STDIN_FILENO, GOLD, 8);
-	write(STDIN_FILENO, NAME, ft_strlen(NAME));
-	write(STDIN_FILENO, NONECOLOR, 5);
+	write(STDOUT_FILENO, GOLD, 8);
+	write(STDOUT_FILENO, NAME, ft_strlen(NAME));
+	write(STDOUT_FILENO, NONECOLOR, 5);
 	ret = get_next_line(1, &line);
 	while (ret != -1)
 	{
 		command = parser(line);
 		executor(command);
 		printf("Hi from main, command:\n\"%s\"\n", command);
-		write(STDIN_FILENO, GOLD, 6);
-		write(STDIN_FILENO, NAME, ft_strlen(NAME));
-		write(STDIN_FILENO, NONECOLOR, 5);
+		write(STDOUT_FILENO, GOLD, 8);
+		write(STDOUT_FILENO, NAME, ft_strlen(NAME));
+		write(STDOUT_FILENO, NONECOLOR, 5);
 		ret = get_next_line(1, &line);
 	}
 }
