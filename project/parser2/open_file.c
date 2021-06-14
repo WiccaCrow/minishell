bae033080fcda8f26538c8d14aeb6066d28dfffe
grvelva <grvelva@student.21-school.ru>
2021-06-14 16:53:46 +0300

    открытые ранее фдшки закрываются

--- project/parser2/open_file.c
+++ project/parser2/open_file.c
@@ -5,14 +5,25 @@ int open_file(t_command *command, char *filename)
 	if (command && filename)
 	{
 		if (command->redirect_type & APPEND)
+		{
+			if (command->output_fd > 1)
+				close(command->output_fd);
 			command->output_fd = open(filename, O_CREAT | O_WRONLY |
-											O_APPEND, 0644);
+												O_APPEND, 0644);
+		}
 		if (command->redirect_type & WRITE)
+		{
+			if (command->output_fd > 1)
+				close(command->output_fd);
 			command->output_fd = open(filename, O_CREAT | O_WRONLY |
-											O_TRUNC, 0644);
-		if (command->redirect_type & READ || \
-                command->redirect_type & LIM_READ)
+												O_TRUNC, 0644);
+		}
+		if (command->redirect_type & READ || command->redirect_type & LIM_READ)
+		{
+			if (command->input_fd > 1)
+				close(command->input_fd);
 			command->input_fd = open(filename, O_RDONLY);
+		}
 		command->redirect_type = command->redirect_type & ~(NO_FILENAME);
 		if (command->output_fd < 0 || command->input_fd < 0)
 		{
