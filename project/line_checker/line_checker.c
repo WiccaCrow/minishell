5700b2677699b077ae51ea3ba6b6032e5cfa8e13
grvelva <grvelva@student.21-school.ru>
2021-06-19 14:46:15 +0300

    прокачал лайнчекер

--- project/line_checker/line_checker.c
+++ project/line_checker/line_checker.c
@@ -2,41 +2,54 @@
 
 int check_line(t_all *all)
 {
+	int i;
+	
 	if (all && all->line)
 	{
 		if (!*all->line)
 			return (0);
-		if (*all->line == ';')
+		i = 0;
+		while (all->line[i])
 		{
-			if (all->line[1] == ';')
+			if (all->line[i] == ';')
 			{
-				write(STDOUT_FILENO, SYN_ERR, 47);
-				write(STDOUT_FILENO, ";;\'\n", 4);
-				return (0);
-			}
-			else
+				i++;
+				if (all->line[i] == ';')
+				{
+					write(STDOUT_FILENO, SYN_ERR, 47);
+					write(STDOUT_FILENO, ";;\'\n", 4);
+					return (0);
+				} else
+				{
+					i = skip_spaces(all->line, i);
+					if (all->line[i] == ';' || i == 1)
+					{
+						write(STDOUT_FILENO, SYN_ERR, 47);
+						write(STDOUT_FILENO, ";\'\n", 3);
+						return (0);
+					}
+				}
+			} else if (all->line[i] == '|')
 			{
-				write(STDOUT_FILENO, SYN_ERR, 47);
-				write(STDOUT_FILENO, ";\'\n", 3);
-				return (0);
-			}
+				i++;
+				if (all->line[i] == '|')
+				{
+					write(STDOUT_FILENO, SYN_ERR, 47);
+					write(STDOUT_FILENO, "||\'\n", 4);
+					return (0);
+				} else
+				{
+					i = skip_spaces(all->line, i);
+					if (all->line[i] == '|' || i == 1)
+					{
+						write(STDOUT_FILENO, SYN_ERR, 47);
+						write(STDOUT_FILENO, "|\'\n", 3);
+						return (0);
+					}
+				}
+			} else
+				i++;			
 		}
-		else if (*all->line == '|')
-		{
-			if (all->line[1] == '|')
-			{
-				write(STDOUT_FILENO, SYN_ERR, 47);
-				write(STDOUT_FILENO, "||\'\n", 4);
-				return (0);
-			}
-			else
-			{
-				write(STDOUT_FILENO, SYN_ERR, 47);
-				write(STDOUT_FILENO, "|\'\n", 3);
-				return (0);
-			}
-		}
-		
 	}
 	return (1);
 }
