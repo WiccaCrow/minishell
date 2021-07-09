0610dd56de1b4f797318320ba600aa4d9e6d196c
boris <amgboris@ya.ru>
2021-07-09 13:29:07 +0300

    защитил фрии_командс

--- project/parser2/command_list_utils.c
+++ project/parser2/command_list_utils.c
@@ -13,16 +13,19 @@ int free_commands(t_command **commands)
 {
 	t_command *tmp;
 	t_command *tmp2;
-	
-	tmp = *commands;
-	while (tmp)
+
+	if(commands)
 	{
-		close_fd_output_input(tmp);
-		tmp2 = tmp;
-		tmp = tmp->next;
-		free_command(tmp2);
+		tmp = *commands;
+		while (tmp)
+		{
+			close_fd_output_input(tmp);
+			tmp2 = tmp;
+			tmp = tmp->next;
+			free_command(tmp2);
+		}
+		free(commands);
+		commands = NULL;
 	}
-	free(commands);
-	commands = NULL;	
 	return (0);
 }
