da5e536fca2caddc8ee98935e8bd32c2ba0668a2
grvelva <grvelva@student.21-school.ru>
2021-07-11 19:38:37 +0300

    научил гнв ловить фдшник перед редиректом

--- project/line_checker/get_next_word.c
+++ project/line_checker/get_next_word.c
@@ -23,20 +23,41 @@ static int	is_token(const char *line)
 
 static int	is_redirect(char c, const char *tmp_line)
 {
-	if (tmp_line && tmp_line[0] == '>' && c != '>')
+	int	i;
+
+	i = 0;
+	while (tmp_line && ft_isdigit((int)tmp_line[i]))
+		i++;
+	if (tmp_line && tmp_line[i] == '>' && c != '>')
 		return (1);
-	if (tmp_line && tmp_line[0] == '<' && c != '<')
+	if (tmp_line && tmp_line[i] == '>' && tmp_line[i + 1] == '>')
+		return (1);
+	if (tmp_line && tmp_line[i] == '<' && c != '<')
+		return (1);
+	if (tmp_line && tmp_line[i] == '<' && tmp_line[i + 1] == '<')
 		return (1);
 	return (0);
 }
 
+int is_word_num(const char *word)
+{
+	int	i;
+
+	i = 0;
+	while (word && ft_isdigit((int)word[i]))
+		i++;
+	if (word[i] == '>' || word[i] == '<')
+		i++;
+	return (word[i] == 0);
+}
+
 int is_word_ended(char c, const char *tmp_line, int flag)
 {
-	if (is_redirect(c, tmp_line) && !flag)
-		return (1);
 	if (is_token(tmp_line) && !flag)
 		return (1);
-	if (tmp_line && *tmp_line && !ft_strchr(">;|&", (int)*tmp_line) && \
+	if (is_redirect(c, tmp_line) && !flag)
+		return (1);
+	if (tmp_line && *tmp_line && !is_word_num(tmp_line) && \
 		ft_strchr(">;|&", (int)c) && !flag)
 		return (1);
 	return (0);
@@ -62,9 +83,7 @@ int	get_next_word_lc(char *line, int i, char **tmp_line)
 				flag = flag & ~(SHIELD);
 		}
 		else
-		{
 			return (i);
-		}
 		i++;
 	}
 	return (i);
