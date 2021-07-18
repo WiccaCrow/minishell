f7d199ace431e9c0b4ab8fab4824d300ce678504
grvelva <grvelva@student.21-school.ru>
2021-07-18 20:06:26 +0300

    занормил

--- project/parser2/wildcard_handle.c
+++ project/parser2/wildcard_handle.c
@@ -22,9 +22,9 @@ int	add_wclist_to_args(t_list **args, t_list **wc_list)
 int	is_wildcard(const char *word)
 {
 	int	i;
-	int flag;
-	
-	if(word)
+	int	flag;
+
+	if (word)
 	{
 		flag = 0;
 		i = -1;
@@ -34,8 +34,8 @@ int	is_wildcard(const char *word)
 				flag = flag | SHIELD;
 			else if (word[i] == '\"' && !(flag & SHIELD) && !(flag & QUOTE))
 				flag = flag ^ DOUBLE_QUOTE;
-			else if (word[i] == '\'' && !(flag & SHIELD) &&
-					 !(flag & DOUBLE_QUOTE))
+			else if (word[i] == '\'' && !(flag & SHIELD) && \
+				!(flag & DOUBLE_QUOTE))
 				flag = flag ^ QUOTE;
 			else if ((word[i] == '*') && !flag)
 				return (1);
@@ -46,35 +46,42 @@ int	is_wildcard(const char *word)
 	return (0);
 }
 
-int	wildcard_handle(t_all *all, t_list ***args)
+int	fill_new_args(t_all *all, t_list ***args, t_list **new_args)
 {
 	t_list	*tmp;
 	t_list	**wc_list;
+
+	tmp = **args;
+	while (tmp)
+	{
+		if (is_wildcard((char *)tmp->content))
+		{
+			wc_list = wildcard_open_read_dir(all->pwd, (char *)tmp->content);
+			if (wc_list)
+			{
+				add_wclist_to_args(new_args, wc_list);
+				ft_free((void **)&(tmp->content));
+			}
+			else
+				ft_lstadd_back(new_args,
+					ft_lstnew(ft_strdup((char *) tmp->content)));
+		}
+		else
+			ft_lstadd_back(new_args,
+				ft_lstnew(((char *) tmp->content)));
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	wildcard_handle(t_all *all, t_list ***args)
+{
 	t_list	**new_args;
 
 	new_args = (t_list **)ft_calloc(1, sizeof(t_list *));
 	if (args && *args && new_args)
 	{
-		tmp = **args;
-		while(tmp)
-		{
-			if(is_wildcard((char *)tmp->content))
-			{
-				wc_list = wildcard_open_read_dir(all->pwd, (char *)tmp->content);
-				if (wc_list)
-				{
-					add_wclist_to_args(new_args, wc_list);
-					ft_free((void **)&(tmp->content));
-				}
-				else
-					ft_lstadd_back(new_args, ft_lstnew(ft_strdup((char *)
-						tmp->content)));
-			}
-			else
-				ft_lstadd_back(new_args, ft_lstnew(((char *)
-				tmp->content)));
-			tmp = tmp->next;
-		}
+		fill_new_args(all, args, new_args);
 		clear_list2(*args);
 		*args = new_args;
 	}
